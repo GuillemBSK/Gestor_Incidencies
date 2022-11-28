43efd6f2eb5aac128aaad205a2c01c3a002d0f0d
GuillemMV <gmagrina1@institutjaumehuguet.cat>
2022-11-28 17:59:09 +0100

    Add imageButton and IC_launcher.

--- app/src/main/java/com/example/gestor_incidencies/NovaIncidencia.java
+++ app/src/main/java/com/example/gestor_incidencies/NovaIncidencia.java
@@ -1,11 +1,13 @@
 package com.example.gestor_incidencies;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +18,8 @@ public class NovaIncidencia extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nova_incidencia);
 
-        Button btnEnvia = findViewById(R.id.ni_enviar);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        ImageButton btnEnvia = findViewById(R.id.ni_enviar);
         btnEnvia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
